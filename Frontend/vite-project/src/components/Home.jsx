f9d1e00db6b5f1bfc37b3988f3ff054c93633c45
Aditya Chaturvedi <achatur755@gmail.com>
2025-04-11 22:55:13 +0530

    Kuch nhi

--- Frontend/vite-project/src/components/Home.jsx
+++ Frontend/vite-project/src/components/Home.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./shared/Navbar";
 import HeroSection from "./HeroSection";
 import CategoryCarousel from "./CategoryCarousel";
 import LatestJobs from "./LatestJobs";
 import Footer from "./shared/Footer";
 import usegetAllJobs from "./customhooks/usegetAllJobs";
+import { useDispatch } from "react-redux";
+import { setSearchedQuery } from "../redux/jobSlice";
 
 export default function Home() {
+  const dispatch = useDispatch();
   usegetAllJobs();
+  useEffect(() => {
+    dispatch(setSearchedQuery(""));
+  }, []);
   return (
     <div>
       <Navbar></Navbar>
